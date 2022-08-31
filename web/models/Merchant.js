a41d70fc3404889ce5c607645d6d9669d4bbdd99
zeeshan <zeesa18.2@gmail.com>
2022-08-31 13:53:28 +0500

    added Merchant Model, which will contains static tabs of different merchants

---
+++ web/models/Merchant.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+const merchantSchema = mongoose.Schema({
+  shop: {
+    type: String,
+  },
+  tabs: [
+    {
+      title: {
+        type: String,
+        required: [true, 'Please enter title.'],
+      },
+      label: {
+        type: String,
+      },
+      description: {
+        type: String,
+        required: [true, 'Please write some description.'],
+      },
+      assignedTo: {
+        type: String,
+        enum: ['All Products'],
+        required: [true, 'Please select Assigned To'],
+      },
+    },
+  ],
+});
+
+const MerchantModel = mongoose.model('Merchant', merchantSchema);
+
+export default MerchantModel;
