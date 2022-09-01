bd7fabbe9b884c59bc61a9aeccc3218b94524896
zeeshan <zeesa18.2@gmail.com>
2022-09-01 12:02:59 +0500

    Listing all tabs

--- web/frontend/components/TabsListing.jsx
+++ web/frontend/components/TabsListing.jsx
@@ -1,9 +1,24 @@
 import { useNavigate } from '@shopify/app-bridge-react';
 import { IndexTable, TextStyle, Card, Button } from '@shopify/polaris';
 import React from 'react';
-
+import { useEffect } from 'react';
+import { useAuthenticatedFetch } from '../hooks';
+import { useState } from 'react';
+import ToggleButton from 'react-toggle-button';
 export function TabsListing() {
+  const fetch = useAuthenticatedFetch();
   const navigate = useNavigate();
+  const [tabs, setTabs] = useState([]);
+  const [isTabsChange, setIsTabsChange] = useState(false);
+
+  const [toggleBtn, setToggleBtn] = useState(false);
+
+  useEffect(async () => {
+    const response = await fetch('/api/tabs');
+    const data = await response.json();
+    setTabs([...data.tabs]);
+  }, [isTabsChange]);
+
   const customers = [
     {
       id: '3411',
@@ -27,12 +42,12 @@ export function TabsListing() {
     plural: 'customers',
   };
 
-  const rowMarkup = customers.map(({ id, name, location }, index) => (
-    <IndexTable.Row id={id} key={id} position={index}>
+  const rowMarkup = tabs?.map(({ _id, title, assignedTo, enable }, index) => (
+    <IndexTable.Row id={_id} key={_id} position={index}>
       <IndexTable.Cell>
-        <TextStyle variation='strong'>{name}</TextStyle>
+        <TextStyle variation='strong'>{title}</TextStyle>
       </IndexTable.Cell>
-      <IndexTable.Cell>{location}</IndexTable.Cell>
+      <IndexTable.Cell>{assignedTo}</IndexTable.Cell>
       <IndexTable.Cell>
         <Button primary>Edit</Button>
         <Button destructive>Delete</Button>
